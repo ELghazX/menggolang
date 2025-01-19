3d2192b5cf3c810d6e34b38778d177bdcc28a62e
Muhammad Ghazali <152017206+ELghazX@users.noreply.github.com>
2025-01-19 15:44:58 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module menggolang
+
+go 1.23.5
---
+++ variable.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var angka = 4
+	name := "ELghaz"
+
+	var (
+		firstname = "Muhammad"
+		lastname  = "Ghazali"
+	)
+	fmt.Println(angka)
+	fmt.Println(name)
+	fmt.Println(firstname, lastname)
+}
