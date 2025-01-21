697760369e94ed05f9fb0bf5b1114c7cbf8b87cb
Muhammad Ghazali <152017206+ELghazX@users.noreply.github.com>
2025-01-21 21:58:48 +0800

    refactor(variable): update variable declaration

--- variable.go
+++ variable.go
@@ -5,7 +5,9 @@ import (
 )
 
 func main() {
-	var angka = 4
+	// membuat variabel langsung inisialisasi nilai awal
+	angka := 4
+	// bisa disingkat begini
 	name := "ELghaz"
 
 	const (
@@ -20,4 +22,15 @@ func main() {
 	fmt.Println(angka)
 	fmt.Println(name)
 	fmt.Println(firstname, lastname)
+
+	// oke inii lebih jelasnya
+	// untuk membuat variabel global di luar function manapun
+	// membuat nya wajib seperti ini
+	// var namaVar dataType = string
+	var namaVar string
+	var namaVar1 string = "Apapun"
+	// tapi jika ada di dalam function termasuk func main
+	// kita bisa singkat seperti ini jika ada data yang inisialisasi
+	// namaVar := "Apa saja"
+	// ingat ini harus di dalam suatu function atau variabel lokal
 }
